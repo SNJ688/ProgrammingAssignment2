40fff07374767f96ab4228b1080d147e7cc8becf
SNJ688 <36209050+SNJ688@users.noreply.github.com>
2018-10-26 11:32:18 +0200

    Solution for programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains two functions for creating a "special" matrix that is capable of storing/caching its inverse
+## - makeCacheMatrix
+## - cacheSolve
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## makeCacheMatrix: function that makes a special matrix object capable of storing a cached inverse matrix
+makeCacheMatrix <- function(x = matrix())
+{
+  # set cache variable to NULL when special matrix is created
+  inverse <- NULL
+  
+  # functions for setting and getting the actual matrix, and setting and getting the cached inverse
+  set <- function(m)
+  {
+    x <<- m
+    # reset the cached inverse since we have a new matrix now
+    inverse <<- NULL
+  }
+  get <- function()
+  {
+    x
+  } 
+  getinverse <- function()
+  {
+    inverse
+  }
+  setinverse <- function(i)
+  {
+    inverse <<- i
+  }
+  
+  # return a list of the functions
+  list(set = set, get = get, getinverse = getinverse, setinverse = setinverse)
+  
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve: solves the supplied "special" matrix unless a cached inverse is already set in the matrix object
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  # First check if an inverse is already set
+  i <- x$getinverse()
+  if(!is.null(i))
+  {
+    # inverse is set for the matrix, so lets reuse this cached value
+    print("Using cached value:")
+    return(i)
+  } else
+  {
+    # inverse is not set for the matrix, so lets solve it
+    print("No cached value, solving:")
+    s = solve(x$get())
+    x$setinverse(s)
+    return(s)
+  }
 }
+
